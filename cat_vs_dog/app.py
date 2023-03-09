0d4c6c43ea5b633b4f0b74f05f62ca47298c5e3d
dev-dhairysheel <103884089+dev-dhairysheel@users.noreply.github.com>
2023-03-09 22:05:35 +0530

    Update app.py

--- cat_vs_dog/app.py
+++ cat_vs_dog/app.py
@@ -1,21 +1,15 @@
-import streamlit as st
-import numpy as np
-import cv2
 import pickle
-import tempfile
+import cv2
+import numpy as np
+import streamlit as st
 from PIL import Image
 
-# Load the pre-trained model
 model = pickle.load(open('cat_vs_dog/cats_vs_dogs.pkl', 'rb'))
 
-# Define the predict_image function to make predictions on new images
-def predict_image(uploaded_file):
-    # Save the uploaded file to a temporary location
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        tmp_file.write(uploaded_file.read())
-        imagepath = tmp_file.name
-
+#to predict new images 
+def predict_image(imagepath):
     img_array = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
+    print(f"img_array: {img_array}")
     new_array = cv2.resize(img_array, (60, 60))
     input_image = np.array(new_array).reshape(-1, 60, 60, 1) / 255.0
     result = model.predict(input_image)
@@ -35,22 +29,11 @@ def predict_image(uploaded_file):
             prediction = 'none'
     return prediction
 
-# Set the title and the sidebar heading of the app
-st.title("Cat or Dog Classifier")
-st.sidebar.title("Upload Image")
-
-# Create the file uploader to upload new images
-uploaded_file = st.sidebar.file_uploader(label="Upload an image", type=["jpg", "jpeg", "png"])
-
-# Check if an image has been uploaded
+st.title("Cat vs Dog Classifier")
+st.set_option('deprecation.showfileUploaderEncoding', False)
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
-    # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded image", use_column_width=True)
-
-    # Make a prediction on the uploaded image
+    st.image(image, caption='Uploaded Image', use_column_width=True)
     prediction = predict_image(uploaded_file)
-    if prediction == "none":
-        st.warning("Sorry, we could not make a prediction on this image.")
-    else:
-        st.success(f"The uploaded image is a {prediction}.")
+    st.write(f"Prediction: {prediction}")
