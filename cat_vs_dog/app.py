8fcdab30bd8ab5debc4944c958364ff74107de31
dev-dhairysheel <103884089+dev-dhairysheel@users.noreply.github.com>
2023-03-09 22:02:46 +0530

    Update app.py

--- cat_vs_dog/app.py
+++ cat_vs_dog/app.py
@@ -1,13 +1,20 @@
-import pickle
-import cv2
-import numpy as np
-from PIL import Image
 import streamlit as st
+import numpy as np
+import cv2
+import pickle
+import tempfile
+from PIL import Image
 
+# Load the pre-trained model
 model = pickle.load(open('cat_vs_dog/cats_vs_dogs.pkl', 'rb'))
 
-#to predict new images 
-def predict_image(imagepath):
+# Define the predict_image function to make predictions on new images
+def predict_image(uploaded_file):
+    # Save the uploaded file to a temporary location
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.read())
+        imagepath = tmp_file.name
+
     img_array = cv2.imread(imagepath, cv2.IMREAD_GRAYSCALE)
     new_array = cv2.resize(img_array, (60, 60))
     input_image = np.array(new_array).reshape(-1, 60, 60, 1) / 255.0
@@ -26,25 +33,24 @@ def predict_image(imagepath):
         probability = probability*100
         if probability < 90:
             prediction = 'none'
+    return prediction
 
-# Set the title of the app
-st.title("Cat vs. Dog Classifier")
+# Set the title and the sidebar heading of the app
+st.title("Cat or Dog Classifier")
+st.sidebar.title("Upload Image")
 
-# Create a file uploader widget in Streamlit
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# Create the file uploader to upload new images
+uploaded_file = st.sidebar.file_uploader(label="Upload an image", type=["jpg", "jpeg", "png"])
 
-# If an image is uploaded
+# Check if an image has been uploaded
 if uploaded_file is not None:
-    # Display the image on the app
+    # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.image(image, caption="Uploaded image", use_column_width=True)
 
-    # Make a prediction and display the result
+    # Make a prediction on the uploaded image
     prediction = predict_image(uploaded_file)
-    if prediction == 'dog':
-        st.write("I'm pretty sure that's a dog!")
-    elif prediction == 'cat':
-        st.write("I'm pretty sure that's a cat!")
+    if prediction == "none":
+        st.warning("Sorry, we could not make a prediction on this image.")
     else:
-        st.write("I'm not sure what that is...")
-
+        st.success(f"The uploaded image is a {prediction}.")
